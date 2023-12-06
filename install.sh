fc1841e376660b0457f723fa3700f131810f5950
Ewerton Lima <ehzcl@outlook.com>
2023-12-06 08:25:51 -0300

    desafio da semana

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+sudo yum install -y nginx 1>/dev/null
+[ -e /usr/sbin/nginx ] && sudo service start nginx
+
+echo "PS1='Ewerton@server$ '" >> $HOME/.bashrc
+
+sudo wget -q -O /usr/share/nginx/html/index.html https://raw.githubusercontent.com/ehzcl/desafio-logica-tok/devops/index.html
+sudo wget -q -O /usr/share/nginx/html/style.css  https://raw.githubusercontent.com/ehzcl/desafio-logica-tok/devops/style.css
+
